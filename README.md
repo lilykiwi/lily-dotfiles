90e29fc100f0a900427795f16ac51161b8896732
Charlotte <48696244+charlottie822@users.noreply.github.com>
2020-03-21 18:26:00 +0000

    update configs, install script, and readme

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ git clone https://github.com/tofustardust/dotfiles.git
 cd dotfiles
 ./install.sh
 ```
+
+## Adding configs
+
+Put a file in the repository, then add a line to `install.sh` constructed like this:
+
+```sh
+create_link [filename] [target-dir] #[description]
+```
+
+For a dotfile, it's best to have the filename not include the dot, then add a dot to the target dir. An example of this is `create_link zshrc ~/.`, which symlinks `~/.zshrc` .
--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
-# should return ~/GitHub/dotfiles
-SOURCEDIR=$(readlink -f $(dirname $0))
+# should return ~/GitHub/dotfiles on my system
+dir=$(readlink -f $(dirname $0))
 
 # deletes and symlinks
 create_link () {
-  echo from $SOURCEDIR/$1 to $2$1
-  rm -f $2$1 && ln -s $SOURCEDIR/$1 $2$1
+  echo from $dir/$1 to $2$1
+  rm -f $2$1 && ln -s $dir/$1 $2$1
 }
 
-# /. means dotfile, / means regular file
-# function    $1                 $2
-create_link   zshrc              ~/.
-create_link   kitty.conf         ~/.config/kitty/
-create_link   colors.conf        ~/.config/kitty/
-create_link   gateau.zsh-theme   ~/.oh-my-zsh/themes/
+# function    $1                 $2                     # details
+create_link   zshrc              ~/.                    # ZSH config (oh-my-zsh)
+create_link   kitty.conf         ~/.config/kitty/       # Kitty teminal config
+create_link   colors.conf        ~/.config/kitty/       # Kitty teminal config
+create_link   gateau.zsh-theme   ~/.oh-my-zsh/themes/   # ZSH theme
+create_link   compton.conf       ~/.config/             # For use with XFCE
+create_link   gtkrc              ~/.config/             # For use with XFCE
+create_link   gtkrc-2.0          ~/.config/             # For use with XFCE

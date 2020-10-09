4c1c41cae3a639f36056d5d38c7cb5cde2a0bd8d
Lyla <48696244+summerysaturn@users.noreply.github.com>
2020-10-09 11:56:18 +0100

    feat: :sparkles: add emoji for CI codes

--- devmoji.config.js
+++ devmoji.config.js
@@ -7,5 +7,35 @@ module.exports = {
       description:
         "meta changes to repository, like, funding.yml and image changes.",
     },
+    {
+      code: "CI401",
+      emoji: ":building_construction:",
+      description: "Introduction to Programming",
+    },
+    {
+      code: "CI405",
+      emoji: ":computer:",
+      description: "Computing Technologies",
+    },
+    {
+      code: "CI410",
+      emoji: ":video_game:",
+      description: "Introduction to Game Design and Development",
+    },
+    {
+      code: "CI435",
+      emoji: ":globe_with_meridians:",
+      description: "Introduction to Web Development",
+    },
+    {
+      code: "CI465",
+      emoji: ":mortar_board:",
+      description: "Working in the Digital and Games Industries",
+    },
+    {
+      code: "CI474",
+      emoji: ":art:",
+      description: "Introduction to 3D Modelling and Animation",
+    },
   ],
 };

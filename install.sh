ba3f04fbaae79569889d3e4e4d0e8338657102c4
Lily <48696244+summerysaturn@users.noreply.github.com>
2021-08-25 21:23:52 +0100

    up-to-date commit

--- install.sh
+++ install.sh
@@ -12,26 +12,19 @@ create_link () {
   rm -rf $2$1 && ln -s $dir/$1 $2$1
 }
 
-dconf write /org/gnome/shell/extensions/panel-date-format/format "'SOL %j  /  %y-%m-%d  /  %R  /  %:z %Z'"
-
 git config --global alias.all '!f() { ls -R -d */.git | xargs -I{} bash -c "echo {} && git -C {}/../ $1"; }; f'
 
 # function  $1                $2   # details
 create_link alacritty.yml     ~/.  # Alacritty terminal config
 create_link zshrc             ~/.  # ZSH config (oh-my-zsh)
 create_link p10k.zsh          ~/.  # p10k-zsh
-create_link xinitrc           ~/.  # .xinitrc to load ~/.Xmodmap
-create_link Xmodmap           ~/.  # .Xmodmap for Keychron K6
-create_link xinitrc           ~/.  # Initscript for Xmodmap
-create_link vscode-workspaces ~/.  # vscode-workspaces
-create_link sxhkdrc           ~/.config/sxhkd # sxhkd
 
 echo "please run the following scripts (if you haven't already):"
 if [ -e ~/.oh-my-zsh ]
 then
   echo "Oh-My-Zsh is already installed! good!"
 else
-  echo "sh -c \"$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)\""
+  echo "sh -c \"\$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)\""
   echo "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \${ZSH_CUSTOM:-\$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 fi
 if [ -e ~/.emacs.d ]

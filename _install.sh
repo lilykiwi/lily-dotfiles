1197bdc71d164ad7b1d8d0f5351fc9b5b419753e
Lily <48696244+summerysaturn@users.noreply.github.com>
2021-12-10 10:17:15 +0000

    feat: latest dotfiles

--- _install.sh
+++ _install.sh
@@ -24,6 +24,10 @@ create_link polybar/launch.sh launch.sh     ~/.config/polybar/ # polybar
 create_link rofi/LilyRofi.rasi  LilyRofi.rasi   /usr/share/rofi/themes/   # rofi
 create_link rofi/config.rasi  config.rasi   ~/.config/rofi/   # rofi
 create_link compton/compton.conf  compton.conf   ~/.config/   # compton
+create_link openbox/rc.xml    rc.xml        ~/.config/openbox/ # openbox
+create_link openbox/menu.xml    menu.xml        ~/.config/openbox/ # openbox
+create_link betterlockscreenrc    betterlockscreenrc        ~/.config/ # openbox
+
 
 echo '      run:
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
